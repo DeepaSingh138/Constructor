47ca6fee9738ae372ef931d9c8a4d8dd00a8f26d
DeepaSingh138 <deepadelight001@gmail.com>
2024-07-11 16:58:30 +0530

    Constructor Assignment

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Constructor_assignment{
+public class Program
+{
+    public static void Main(string[] args)
+    {
+            Constructor_assignment.Student S1 = new Student();
+               Student S2 = new Student("deepa",20);
+            Console.WriteLine($"Student is {S2.name} and enrollment number is {S2.rollno}");
+
+    }
+
+}
+    }
+
---
+++ Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_assignment
+{
+    public class Student
+    {
+       
+        
+            public string name;
+            public int rollno;
+            public Student()
+            {
+                Console.WriteLine("Default Constructor invoked");
+            }
+            public Student(string fullname, int rollnum)
+            {
+                name = fullname;
+                rollno = rollnum;
+                Console.WriteLine("Parameterized Constructor is invoked");
+            }
+
+        }
+    }
+
